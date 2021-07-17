e5b345060b86506c442155a3ca181fca1d49e55c
David Itzhak <daviditzhak7@gmail.com>
2021-07-17 16:52:38 +0300

    index.js has two endpoints.

--- index.js
+++ index.js
@@ -3,18 +3,25 @@ const fs = require('fs');
 const path = require('path');
 
 const server = http.createServer((request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  const path = path.join('..', 'public', 'index.html')
-  response.end(fileRider(path, (err , content) => {
-    if(err){
-      throw err;
-    }
-    const data = Buffer.from(content);
-    return  data.toString();
-  }));
-});
 
-const fileRider = fs.readFile(filePath, cb);
+  if (request.url === '/') {
+    fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
+      if (err) {
+        throw err;
+      }
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      response.end(data);
+    })
+  } else if (request.url === '/contact') {
+    fs.readFile(path.join(__dirname, 'public', 'contact.html'), (err, data) => {
+      if (err) {
+        throw err;
+      }
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      response.end(data);
+    })
+  }
+});
 
 server.listen(30000, () => {
   console.log('Server has been started');
